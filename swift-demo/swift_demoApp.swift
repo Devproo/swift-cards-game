3fc4cf378cf48833efa7f0ec80bde287de7c4135
devproo <ipeerless0@gmail.com>
2023-03-05 12:01:58 +0200

    Initial Commit

---
+++ swift-demo/swift_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_demoApp.swift
+//  swift-demo
+//
+//  Created by ipeerless on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
